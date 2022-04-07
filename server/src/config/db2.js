d651073b32253c28f2ba414dd87ec0065db9d3f9
eastpeople0301@gmail.com <sindongmin@sindongmin-ui-MacBookAir.local>
2022-04-07 09:48:32 +0900

    db.js 비밀번호 노출로 인해 히스토리 삭제

---
+++ server/src/config/db2.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DATABASE,
+});
+
+connection.connect();
+
+module.exports = connection;
