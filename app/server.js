1c24075e501500d886681b06607113bfed7374cd
eastpeople0301@gmail.com <sindongmin@sindongmin-ui-MacBookAir.local>
2022-05-21 16:17:37 +0900

    commite before deploy

--- app/server.js
+++ app/server.js
@@ -11,6 +11,7 @@ app.use(require('cors')());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 //Route
 app.use('/api', homeRouter);
